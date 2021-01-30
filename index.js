c7d03b86d3b7b3e1ab9f78f9f3363923b43ed3a8
Ankit Gawande <ankitgawande651@gmail.com>
2021-01-30 10:33:51 +0530

    Hello Project

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import Helloworld from './Helloworld';
+
+AppRegistry.registerComponent(appName, () => App);
